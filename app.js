7629cb3340531515c915c36a848a4419e49eaf9e
kguinto <klogold1@gmail.com>
2018-02-12 15:18:06 -0800

    Added click event listener stubs to board and fixed alignment of place on piece render

--- app.js
+++ app.js
@@ -8,16 +8,26 @@ window.board =
 window.onload = (e) => {
 
   console.log('Loaded tic-tac-toe app');
+  console.table(window.board);
 
   document.getElementById('reset-button').addEventListener('click', (e)=>{
     e.preventDefault();
     console.log('Resetting board.');
   });
+
+  let places = document.getElementsByClassName('place');
+
+  
+  Array.from(places).forEach(place => {
+      place.addEventListener('click', function (e) {
+        console.log(this);
+      });
+    });
+
+  
 };
 
 
 window.controller = {
-
-
 
 };
