b96f1d2a7425f2e7a684a53f88236b080241d852
kguinto <klogold1@gmail.com>
2018-02-12 12:30:35 -0800

    Stubbed out app architecture

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-console.log('Loaded tic-tac-toe app');
+/* Model */
+window.board =
+[[ ' ', ' ', ' '],
+ [ ' ', ' ', ' '],
+ [ ' ', ' ', ' ']]; 
+
+/* View */
+window.onload = (e) => {
+
+  console.log('Loaded tic-tac-toe app');
+
+  document.getElementById('reset-button').addEventListener('click', (e)=>{
+    e.preventDefault();
+    console.log('Resetting board.');
+  });
+};
+
+
+window.controller = {
 
 
 
-
+};
