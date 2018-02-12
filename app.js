8eef6ebe8e28cab2aaa49a6a9fe599c7d0901f53
kguinto <klogold1@gmail.com>
2018-02-12 15:40:29 -0800

    Implemented setPiece function

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 /* Model */
-window.board =
-[[ ' ', ' ', ' '],
- [ ' ', ' ', ' '],
- [ ' ', ' ', ' ']]; 
+
+window.model = {
+  board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+};
+
 
 /* View */
 window.onload = (e) => {
@@ -20,7 +21,7 @@ window.onload = (e) => {
   
   Array.from(places).forEach(place => {
       place.addEventListener('click', function (e) {
-        console.log(this);
+        window.controller.setPiece('X', this.id);
       });
     });
 
@@ -29,5 +30,27 @@ window.onload = (e) => {
 
 
 window.controller = {
+
+  rowMap : { 'A': 0, 'B': 1, 'C': 2 },
+
+  pieceMap : { 'X': 1, 'O': 2},
+  
+  /**
+   *  Writes 1 or 2 to a place on the board model, given:
+   *   piece -- Single character string 'X' or 'O'
+   *   place -- Two character string representing the row and column ('A0', 'A1', 'A2', 'B0', etc....)
+   */
+  setPiece: function (piece, place) {
+    console.log('setting piece', piece, 'at place', place);
+    if(place.length === 2 && (piece === 'X' || piece === 'O') ) {
+      let row = window.controller.rowMap[place[0]];
+      let column = place[1];
+
+      window.model.board[row][column] = window.controller.pieceMap[piece];
+      console.table(window.model.board); // TODO delete
+    } else {
+      console.error('Something went wrong. Incorrect usage of window.controller.setPiece function.');
+    }
+  }
 
 };
