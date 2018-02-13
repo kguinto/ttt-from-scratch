c579b2cf93e45fa3df107203ad0c23b5f93e5a4d
kguinto <klogold1@gmail.com>
2018-02-12 16:18:06 -0800

    Refactor to clearer MVC architecture

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
+window.app = app = {};
+
 /* Model */
-
-window.model = {
-  board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-};
-
-
-/* View */
-window.onload = (e) => {
-
-  console.log('Loaded tic-tac-toe app');
-  console.table(window.board);
-
-  document.getElementById('reset-button').addEventListener('click', (e)=>{
-    e.preventDefault();
-    console.log('Resetting board.');
-  });
-
-  let places = document.getElementsByClassName('place');
-
-  
-  Array.from(places).forEach(place => {
-      place.addEventListener('click', function (e) {
-        window.controller.setPiece('X', this.id);
-      });
-    });
-
-  
-};
-
-
-window.controller = {
+app.model = {
+  board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
 
   rowMap : { 'A': 0, 'B': 1, 'C': 2 },
 
   pieceMap : { 'X': 1, 'O': 2},
+};
+
+
+/* View */
+app.view = {
+  init : (e) => {
+    console.log('Loaded tic-tac-toe app');
+    console.table(app.board);
+  
+    document.getElementById('reset-button').addEventListener('click', (e)=>{
+      e.preventDefault();
+      console.log('Resetting board.');
+    });
+  
+    let places = document.getElementsByClassName('place');
+    
+    Array.from(places).forEach(place => {
+      place.addEventListener('click', function (e) {
+        app.controller.setPiece('X', this.id);
+      });
+    });
+  }
+};
+
+
+
+
+
+
+app.controller = {
   
   /**
    *  Writes 1 or 2 to a place on the board model, given:
@@ -43,13 +46,13 @@ window.controller = {
   setPiece: function (piece, place) {
     console.log('setting piece', piece, 'at place', place);
     if(place.length === 2 && (piece === 'X' || piece === 'O') ) {
-      let row = window.controller.rowMap[place[0]];
+      let row = app.model.rowMap[place[0]];
       let column = place[1];
 
-      window.model.board[row][column] = window.controller.pieceMap[piece];
-      console.table(window.model.board); // TODO delete
+      app.model.board[row][column] = app.model.pieceMap[piece];
+      console.table(app.model.board); // TODO delete
     } else {
-      console.error('Something went wrong. Incorrect usage of window.controller.setPiece function.');
+      console.error('Something went wrong. Incorrect usage of app.controller.setPiece function.');
     }
   }
 
